48958c53904b83c9683ee7d0bf380f3647757961
Himanshu Raj <71289405+himansh231104@users.noreply.github.com>
2024-03-07 21:07:54 +0530

    Courses Pages Added

--- app.py
+++ app.py
@@ -10,12 +10,28 @@ def about():
     return render_template('base.html')
 
 @app.route('/portfolio')
-def contact():
+def portfolio():
     return render_template('portfolio-details.html')
 
+@app.route('/user-profile')
+def user_profile():
+    return render_template('user-profile.html')
+
 @app.route('/python')
-def course():
-    return render_template('python.html')
+def python():
+    return render_template('Courses/python.html')
+
+@app.route('/java')
+def java():
+    return render_template('Courses/java.html')
+
+@app.route('/c')
+def c():
+    return render_template('Courses/c.html')
+
+@app.route('/cpp')
+def cpp():
+    return render_template('Courses/cpp.html')
 
 
 
